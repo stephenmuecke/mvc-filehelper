e2c80639a6d94895e618b0d7e420774b4a1f3c7a
stephenmuecke <smuecke@optusnet.com.au>
2017-03-03 15:07:32 +1030

    Update FileAttachment.cs

--- Models/FileAttachment.cs
+++ Models/FileAttachment.cs
@@ -4,50 +4,6 @@ namespace Sandtrap.Web.Models
 {
 
     /// <summary>
-    /// Describes the status of a file.
-    /// </summary>
-    public enum FileAttachmentStatus
-    {
-        Added = 1,
-        Deleted = -1
-    }
-
-    /// <summary>
-    /// Describes the properties of a file.
-    /// </summary>
-    public interface IFileAttachment
-    {
-        /// <summary>
-        /// Gets or sets the ID of the file.
-        /// </summary>
-        int? ID { get; set; }
-        /// <summary>
-        /// Gets of sets the name.
-        /// </summary>
-        string FileName { get; set; }
-        /// <summary>
-        /// Gets or sets the file directory.
-        /// </summary>
-        string Directory { get; set; }
-        /// <summary>
-        /// Gets or sets the file display name
-        /// </summary>
-        string DisplayName { get; set; }
-        /// <summary>
-        /// Gets of sets the file size (in bytes).
-        /// </summary>
-        int? Size { get; set; }
-        /// <summary>
-        /// Gest or sets the file status.
-        /// </summary>
-        FileAttachmentStatus Status { get; set; }
-        /// <summary>
-        /// Gets or sets the file uploaded by the client.
-        /// </summary>
-        HttpPostedFileBase File { get; set; }
-    }
-
-    /// <summary>
     /// Sample concrete implementation of IFileAttachment.
     /// </summary>
     public class FileAttachment : IFileAttachment
