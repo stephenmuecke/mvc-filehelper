474f16a32141a63f01c8749ba88e77f604e7902e
stephenmuecke <smuecke@optusnet.com.au>
2017-03-03 15:08:45 +1030

    Add files via upload

---
+++ FileHelper.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using Sandtrap.Web.Models;
+
+
+namespace Sandtrap.Web.Utilities
+{
+
+    /// <summary>
+    /// Utility methods for saving, deleting and downloading files on the server.
+    /// </summary>
+    public static class FileHelper
+    {
+
+        #region .Declarations 
+
+        private const string nullDirectory = "The Directory cannot be null.";
+        const string nullFileName = "The FileName cannot be null.";
+        const string nullPath = "The Path cannot be null.";
+
+        #endregion
+
+        #region .Methods 
+
+        /// <summary>
+        /// Saves a files to the server.
+        /// </summary>
+        /// <param name="attachments">
+        /// A collection of `IFileAttachment` objects.
+        /// </param>
+        /// <param name="virtualPath">
+        /// The virtual path to save the files.
+        /// </param>
+        /// <param name="deleteFiles">
+        /// A value indicating if files marked for deletion should be deleted from the server.
+        /// </param>
+        public static void Save(IEnumerable<IFileAttachment> attachments, string virtualPath, bool deleteFiles)
+        {
+            // Get the physical path
+            string physicalPath = HttpContext.Current.Server.MapPath(virtualPath);
+            // string physicalPath = HostingEnvironment.MapPath(path);
+            if (!Directory.Exists(physicalPath))
+            {
+                Directory.CreateDirectory(physicalPath);
+            }
+            foreach (IFileAttachment attachment in attachments)
+            {
+                if (attachment.File != null && attachment.File.ContentLength > 0)
+                {
+                    // Save the file
+                    HttpPostedFileBase file = attachment.File;
+                    string fileName = file.FileName;
+                    string extension = Path.GetExtension(fileName);
+                    // Create unique file name
+                    fileName = string.Format("{0}{1}", Guid.NewGuid(), extension);
+                    // Update properties
+                    attachment.DisplayName = file.FileName;
+                    attachment.Size = (int)Math.Round(file.ContentLength / 1024F);
+                    attachment.Directory = virtualPath;
+                    attachment.FileName = fileName;
+                    attachment.Status = FileAttachmentStatus.Added;
+                    // Save to server
+                    file.SaveAs(Path.Combine(physicalPath, fileName));
+                }
+                else if (attachment.Status == FileAttachmentStatus.Deleted && deleteFiles)
+                {
+                    // Delete the file from the server
+                    File.Delete(Path.Combine(physicalPath, attachment.FileName));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a FileResult to send the contents of a file to the response.
+        /// </summary>
+        /// <param name="attachment">
+        /// An IFileAttachment containing the properties of the file to download.
+        /// </param>
+        public static FileResult Download(IFileAttachment attachment)
+        {
+            // Validate
+            if (attachment.Directory == null)
+            {
+                throw new ArgumentNullException(nullDirectory);
+            }
+            if (attachment.FileName == null)
+            {
+                throw new ArgumentNullException(nullFileName);
+            }
+            string path = Path.Combine(attachment.Directory, attachment.FileName);
+            if (attachment.DisplayName == null)
+            {
+                attachment.DisplayName = attachment.FileName;
+            }
+            // Return FileResult
+            return Download(path, attachment.DisplayName);
+        }
+
+        /// <summary>
+        /// Returns a FileResult to send the contents of a file to the response.
+        /// </summary>
+        /// <param name="path">
+        /// The virtual path of the file.
+        /// </param>
+        /// <param name="displayName">
+        /// The files display name to be returned to the browser.
+        /// </param>
+        public static FileResult Download(string path, string displayName)
+        {
+            // Validate
+            if (path == null)
+            {
+                throw new ArgumentNullException(nullPath);
+            }
+            if (displayName == null)
+            {
+                displayName = Path.GetFileName(path);
+            }
+            // Get the physical path
+            string physicalPath = HttpContext.Current.Server.MapPath(path);
+            if (!File.Exists(physicalPath))
+            {
+                throw new FileNotFoundException(); // TODO: error message
+            }
+            // Get the files mime type
+            string mineType = MimeMapping.GetMimeMapping(path);
+            // Return FileResult
+            return new FilePathResult(physicalPath, mineType);
+        }
+
+        #endregion
+
+
+
+
+
+
+    }
+
+}
